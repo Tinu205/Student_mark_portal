12337d6f6f5043c03473bbde7aece95ea148914e
Tinu205 <dazleroger404@gmail.com>
2023-11-14 23:02:17 +0530

    Made it work some how

---
+++ Portal/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head>
+    <script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Index</title>
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body{
+            height: 100vh;
+        }
+    </style>
+  </head>
+<body>
+    <div class = "btn d-flex align-items-center justify-content-center">
+        <button type="button" class="btn btn-primary">Student</button>
+        <button type="button" class="btn btn-primary">Faculty</button>
+
+    </div>
+</body>
--- Portal/sign_in.php
+++ Portal/sign_in.php
@@ -1,6 +1,4 @@
 <?php
-    include "libs/load.php";
-    load_style($sign_in)
 ?>
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
@@ -8,18 +6,8 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.111.3">
     <title>Signin Template · Bootstrap v5.3</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
-
-    
-
-    
-
-<link href="/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
       .bd-placeholder-img {
@@ -97,7 +85,7 @@
 
     
     <!-- Custom styles for this template -->
-    <link href="/css/sign_in.css" rel="stylesheet">
+    <link href="css/sign_in.css" rel="stylesheet">
   </head>
   <body class="text-center">
     <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
@@ -172,7 +160,7 @@
       </label>
     </div>
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+    <!-- <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p> -->
   </form>
 </main>
 
