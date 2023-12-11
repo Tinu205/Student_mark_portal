83e0bda8e3dca84f24f6a56dc592033043d7bb81
Tinu205 <dazleroger404@gmail.com>
2023-12-11 11:10:20 +0530

    buttons works need to improve the design

--- Portal/index.php
+++ Portal/index.php
@@ -1,22 +1,31 @@
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
   <head>
-    <script src="../assets/js/color-modes.js"></script>
+  <script src="../assets/js/color-modes.js"></script>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Index</title>
     <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        body{
-            height: 100vh;
-        }
+      body{
+          height: 100vh;
+          }
+      .container {
+        display: flex;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+      }
     </style>
   </head>
 <body>
-    <div class = "btn d-flex align-items-center justify-content-center">
-        <button type="button" class="btn btn-primary">Student</button>
-        <button type="button" class="btn btn-primary">Faculty</button>
-
+    <div class = "container">
+      <form action = "Student.php" method = "get">
+        <button type="submit" class="btn btn-primary m-1">Student</button>
+      </form>
+      <form action = "Faculty.php" method = "get">
+        <button type="submit" class="btn btn-primary m-1">Faculty</button>
+      </form>
     </div>
 </body>
